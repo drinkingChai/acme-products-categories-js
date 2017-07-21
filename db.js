76cfcef0555da95f80489eb0fca648bbab88b0a7
Wasif Zaman <teatocode@gmail.com>
2017-07-21 12:20:04 -0400

    Finish data layer

---
+++ db.js
@@ -0,0 +1,59 @@
+var categories = {};
+
+function createCategory(name) {
+  categories[name] = [];
+}
+
+function deleteCategory(name) {
+  delete categories[name];
+}
+
+function createProduct(category, product) {
+  var cat = categories[category],
+    id = cat.length ? cat[cat.length - 1].id + 1 : 1;
+  cat.push({ name: product, id: id });
+}
+
+function deleteProduct(category, id) {
+  categories[category] = categories[category].filter(function(item) {
+    return item.id != id;
+  })
+}
+
+function getCategoryNames() {
+  var categoryNames = [];
+  for (var k in categories) {
+    categoryNames.push(k);
+  }
+  return categoryNames;
+}
+
+function getProductsByCategory(category) {
+  return categories[category];
+}
+
+
+module.exports = {
+  createCategory: createCategory,
+  deleteCategory: deleteCategory,
+  createProduct: createProduct,
+  deleteProduct: deleteProduct,
+  getCategoryNames: getCategoryNames,
+  getProductsByCategory: getProductsByCategory
+}
+
+
+// test cases
+// createCategory("Foo Category");
+// createProduct("Foo Category", "foo 1");
+// createProduct("Foo Category", "foo 2");
+// createProduct("Foo Category", "foo 3");
+// console.log('added', categories["Foo Category"]);
+// deleteProduct("Foo Category", 2);
+// console.log('del 2', categories["Foo Category"]);
+// createProduct("Foo Category", "foo 2");
+// console.log('add 2', categories["Foo Category"]);
+// deleteCategory("Foo Category");
+// console.log("Drop Foo", categories);
+// console.log(getCategoryNames());
+// console.log(getProductsByCategory("Foo Category"));
--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+const bodyParser= require('body-parser');
+const methodOverride = require('method-override');
+const db = require('./db');
+const categories = require('./routes/categories');
 
 
 const app = express();
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {
+  express: app,
+  noCache: true
+})
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use('/categories', categories);
+
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+
+
 
 
 
